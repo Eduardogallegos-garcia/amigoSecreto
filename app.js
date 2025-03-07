00f8012b89ceee53c65c28de6ea386bedd01854e
Eduardogallegos-garcia <leduardogallegos@hotmail.com>
2025-03-07 17:07:01 -0600

    Se crea la funcionalidad completa de mostrar lista, resultarlos y ocultarlos segun correspondan

--- app.js
+++ app.js
@@ -8,24 +8,34 @@ let amigos=[];
 let contador=0;
 function agregarAmigo(){
     const nombre=inputText.value;
+    lista.style.display="block";
+    resultado.style.display="none";  
     if(nombre==""){
         alert("Por favor, inserte un nombre.")
     }else{
+       
         amigos.push(nombre)
+
         agregarLista();
-        inputText.value="";    
+        inputText.value=""; 
+        console.log(amigos)   
     }    
 }
-function agregarLista(){    
+function agregarLista(){ 
+         
         lista.innerHTML+=`<li>${amigos[contador]}</li>`
         contador++;
 }
 function sortearAmigo(){
     if(amigos.length!=0){
+        resultado.style.display="block";  
         const indice=Math.floor(Math.random()*amigos.length);
-
+        lista.style.display="none";
         resultado.innerHTML=`<li>El amigo secreto es ${amigos[indice]}</li>`
-        
+        amigos=[];
+        lista.innerHTML="";
+        contador=0;
+        console.log(amigos)
     }else{
         alert("No hay amigos para sortear")
     }
